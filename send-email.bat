3e87fc28e4f0c34be00d57f5fc9dfdb9e2113886
Bo <luofubo@hotmail.com>
2026-04-02 10:25:09 +1300

    clean up

--- send-email.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Send Email Wrapper for Windows
-REM Usage: send-email.bat --to "recipient@example.com" --subject "Subject" --body "Message"
-
-node "%~dp0send-email.js" %*
--- send-report.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Send Report Wrapper for Windows
-REM Usage: send-report.bat report-file.html [recipient@email.com]
-
-node "%~dp0send-report.js" %1 %2
